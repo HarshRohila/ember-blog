c14a015db65524c2a05ae8ad6bbc8f82c5180887
Harsh Rohila <rohilaharsh@gmail.com>
2021-04-19 17:26:51 +0530

    added delete feature for post

--- app/components/posts-list/component.js
+++ app/components/posts-list/component.js
@@ -1,4 +1,9 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 export default class PostsListComponent extends Component {
+	@action
+	onDelete(post) {
+		post.destroyRecord();
+	}
 }
