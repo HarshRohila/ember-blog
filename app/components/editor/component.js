8d4f548cc56d77f56fcea210a1c27a7f7508a807
Harsh Rohila <harsh.rohila@mrisoftware.com>
2021-04-20 18:53:48 +0530

    displaying formatted content for post

--- app/components/editor/component.js
+++ app/components/editor/component.js
@@ -1,3 +1,11 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { toString } from 'ember-froala-editor/helpers/to-string';
 
-export default class EditorComponent extends Component {}
+export default class EditorComponent extends Component {
+	@action
+	onChangeContent(newContent) {
+		newContent = toString(newContent);
+		this.args.onChange?.(newContent);
+	}
+}
