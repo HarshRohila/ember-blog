5ecc7a2009c364d7e65e2d3f91f120d665d285be
Harsh Rohila <rohilaharsh@gmail.com>
2021-04-20 17:46:42 +0530

    added post route

--- app/router.js
+++ app/router.js
@@ -2,10 +2,11 @@ import EmberRouter from '@ember/routing/router';
 import config from 'ember-blog/config/environment';
 
 export default class Router extends EmberRouter {
-  location = config.locationType;
-  rootURL = config.rootURL;
+	location = config.locationType;
+	rootURL = config.rootURL;
 }
 
 Router.map(function () {
-  this.route('create-post');
+	this.route('create-post');
+	this.route('post', { path: '/posts/:post_id' });
 });
