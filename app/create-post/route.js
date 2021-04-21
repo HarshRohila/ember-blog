67739bba9031f8a34b3e2ff40793e9bc501cc0a4
Harsh Rohila <harsh.rohila@mrisoftware.com>
2021-04-21 21:58:09 +0530

    added ember keyboard plugin

--- app/create-post/route.js
+++ app/create-post/route.js
@@ -1,6 +1,17 @@
 import Route from '@ember/routing/route';
 import FormModel from '../utils/FormModel';
+import { inject as service } from '@ember/service';
+
 export default class CreatePostRoute extends Route {
+
+	@service session;
+
+	beforeModel() {
+		if (!this.session.isAuthenticated) {
+			this.transitionTo('application');
+		}
+	}
+
 	model() {
 		const newPost = this.store.createRecord('post');
 		return new FormModel(newPost);
