dd82a07354fce8ad48366a10bcf21c6f382a0fec
Harsh Rohila <harsh.rohila@mrisoftware.com>
2021-04-24 21:26:45 +0530

    added test for form model

--- tests/unit/utils/FormModel-test.js
+++ tests/unit/utils/FormModel-test.js
@@ -1,11 +1,13 @@
-import changeset from 'ember-blog/utils/changeset';
+// import changeset from 'ember-blog/utils/changeset';
+import FormModel from 'ember-blog/utils/FormModel';
 import { module, test } from 'qunit';
+import { Changeset } from 'validated-changeset';
+import { BufferedChangeset } from 'validated-changeset';
 
-module('Unit | Utility | FormModel', function() {
+module('Unit | Utility | FormModel', function () {
 
-  // TODO: Replace this with your real tests.
-  test('it works', function(assert) {
-    let result = changeset();
-    assert.ok(result);
+  test('it returns changeset instance', function (assert) {
+    let result = new FormModel({ test: 'test' });
+    assert.ok(result instanceof BufferedChangeset);
   });
 });
