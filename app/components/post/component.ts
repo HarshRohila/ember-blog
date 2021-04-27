6699b2b98be05ce521af646db8a80df88692f718
Harsh Rohila <harsh.rohila@mrisoftware.com>
2021-04-27 10:10:27 +0530

    added composable helper addon

--- app/components/post/component.ts
+++ app/components/post/component.ts
@@ -1,7 +1,12 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { SessionService } from 'ember-blog/utils/missing-types';
+import { action } from '@ember/object';
 
 export default class PostComponent extends Component {
 	@service session!: SessionService;
+
+	@action onEdit() {
+		console.log('test')
+	}
 }
