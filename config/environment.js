6ae55de28d148d4cc2eb198621d7c2e0872bc38b
Harsh Rohila <harsh.rohila@mrisoftware.com>
2021-04-29 10:08:48 +0530

    changed root url

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'ember-blog',
     environment,
-    rootURL: '/',
+    rootURL: '',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
