aee6d917dbb1ff602bc8bf9e9b26e95a761bdbe2
Harsh Rohila <harsh.rohila@mrisoftware.com>
2021-05-12 01:33:48 +0530

    adding userid while saving

--- app/create-post/controller.ts
+++ app/create-post/controller.ts
@@ -2,12 +2,14 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { Router } from '@ember/routing';
 import { inject as service } from '@ember/service';
+import { SessionService } from 'ember-blog/utils/missing-types';
 import { addTimestampToPost } from 'ember-blog/utils/post';
 import { BufferedChangeset } from 'validated-changeset';
 
 export default class CreatePostController extends Controller {
 
 	@service router!: Router;
+	@service session!: SessionService;
 
 	@action
 	async onCreatePost() {
@@ -20,9 +22,16 @@ export default class CreatePostController extends Controller {
 
 	async savePost(post: BufferedChangeset) {
 		addTimestampToPost(post);
+		this.addUserIdToPost(post);
 		post.execute();
 
 		const newPost = this.store.createRecord('post', post.data);
 		await newPost.save();
+	}
+
+	private addUserIdToPost(post: BufferedChangeset) {
+		const userId = this.session.data.authenticated.user.uid;
+		console.log(userId);
+		post.set('userId', userId);
 	}
 }
